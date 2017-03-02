0049bc6f23f650701665271751c8a00450a5fea1
Justin Travis Mair <travis.mair@gmail.com>
2017-03-02 13:58:21 +0000

    express parameters

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,4 +1,11 @@
 var hotelData=require('../data/hotel-data.json');
+
 module.exports.hotelsGetAll = function(req, res){
     res.json(hotelData);
+};
+
+module.exports.hotelsGetOne = function(req, res){
+    var hotelID = req.params.hotelId;
+    var thisHotel = hotelData[hotelID];
+    res.json(thisHotel);
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+
 var ctrHotels=require('../controllers/hotels.controllers.js');
-
-
 
 router
 .route('/hotels')
 .get(ctrHotels.hotelsGetAll);
 
+router
+.route('/hotels/:hotelId')
+.get(ctrHotels.hotelsGetOne);
 
 module.exports = router;
